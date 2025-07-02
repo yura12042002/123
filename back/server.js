7984be2ad067f8e1522b7aba2b2bda7e0961afc3
yura12042002 <your_email@example.com>
2025-07-03 03:57:16 +0500

    for back

--- back/server.js
+++ back/server.js
@@ -10,6 +10,7 @@ require("./bots/authBot");
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`!!! Server is running on http://localhost:${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`!!! Server is running on http://0.0.0.0:${PORT}`);
 });
